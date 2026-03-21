96d903f29cf0ac2a4095e412afa8e52e65e4347a
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-03-21 23:30:24 +0900

    [Silver III] Title: 두 수의 합, Time: 12 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,40 +1,40 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+vector<int> v;
+
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	int N ;
-	cin >> N;
-	vector<int>v(N);
-	for (int i = 0; i < N; i++) {
-		cin >> v[i];
-	}
+    int N;
+    cin >> N;
 
-	int X;
-	cin >> X;
+    v.resize(N);
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
 
-	sort(v.begin(), v.end());
+    int x;
+    cin >> x;
 
-	int left = 0, right = N-1;
-	int cnt = 0;
-	while (left < right) {
-		if (v[left] + v[right] >= X) {
-			if (v[left] + v[right] == X) {
-				cnt++;
-			}
-			right--;
-		}
-		else {
-			left++;
-		}
-	}
+    sort(v.begin(), v.end());
 
-	cout << cnt <<'\n';
-	return 0;
+    int left = 0, right = N - 1;
+    int cnt = 0;
+    while (left < right) {
+        int sum = v[left] + v[right];
+        if (sum >= x) {
+            right--;
+            if (sum == x) {
+                cnt++;
+            }
+        } else {
+            left++;
+        }
+    }
+    cout << cnt << '\n';
+    return 0;
 }
