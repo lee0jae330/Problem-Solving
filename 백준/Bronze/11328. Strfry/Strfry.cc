28441140cbb1fbe3bf6ae2b520c451de32cc0266
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-03-21 23:25:55 +0900

    [Bronze II] Title: Strfry, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.cc
+++ 백준/Bronze/11328. Strfry/Strfry.cc
@@ -1,30 +1,46 @@
-#include<iostream>
-#include<algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	int N;
-	cin >> N;
+    int N;
+    cin >> N;
+    while (N--) {
+        string a, b;
+        cin >> a >> b;
+        int arr1[26] =
+            {
+                0,
+            },
+            arr2[26] = {
+                0,
+            };
+        for (char c : a) {
+            arr1[c - 'a']++;
+        }
 
-	while (N--) {
-		string a, b;
-		cin >> a >> b;
+        for (char c : b) {
+            arr2[c - 'a']++;
+        }
 
-		sort(a.begin(), a.end());
-		sort(b.begin(), b.end());
+        int flag = 0;
 
-		if (a == b) {
-			cout << "Possible" <<'\n';
-		}
-		else {
-			cout << "Impossible" <<'\n';
-		}
-	}
-	return 0;
+        for (int i = 0; i < 26; i++) {
+            if (arr1[i] != arr2[i]) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (!flag) {
+            cout << "Possible" << '\n';
+        } else {
+            cout << "Impossible" << '\n';
+        }
+    }
+    return 0;
 }
