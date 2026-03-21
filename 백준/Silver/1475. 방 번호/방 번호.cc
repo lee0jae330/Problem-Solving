02a7d1725c73f54ed14b1b0c38c0f07298f4a540
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-03-22 00:05:50 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -1,44 +1,32 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-int main(void)
-{
-	int arr[10] = { 0, };
-	int N,a;
-	scanf("%d", &N);
-	while (1)
-	{
-		if (N >= 10)
-		{
-			a = N % 10;
-			arr[a]++;
-		}
-		else
-		{
-			a = N;
-			arr[a]++;
-			break;
-		}
-		N /= 10;
-	}
-	int new_arr[9];
-	for (int i = 0; i < 9; i++)
-	{
-		if (i == 6)
-		{
-			int tmp = arr[6] + arr[9];
-			if (tmp % 2 == 1)
-				tmp += 1;
-			new_arr[i] = (tmp / 2);
-		}
-		else
-			new_arr[i] = arr[i];
-	}
-	int max = new_arr[0];
-	for (int i = 1; i < 9; i++)
-	{
-		if (max < new_arr[i])
-			max = new_arr[i];
-	}
-	printf("%d\n", max);
-	return 0;
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string str;
+    cin >> str;
+    vector<int> arr(11, 0);
+    for (char c : str) {
+        arr[c - '0']++;
+    }
+
+    int sol = (arr[6] + arr[9]) / 2;
+
+    if ((arr[6] + arr[9]) % 2) {
+        sol++;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (i == 6 || i == 9) {
+            continue;
+        }
+        sol = max(sol, arr[i]);
+    }
+
+    cout << sol << '\n';
+    return 0;
 }
