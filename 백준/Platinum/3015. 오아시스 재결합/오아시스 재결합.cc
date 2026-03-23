eecf072e13dc0841945538f8bf50acd704a633ca
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-03-24 00:52:00 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 72 ms, Memory: 12380 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -1,73 +1,53 @@
-#include<iostream>
-#include<vector>
-#include<stack>
-#include<tuple>
+#include <bits/stdc++.h>
 
 typedef long long ll;
 
 using namespace std;
 
+vector<int> v;
+stack<pair<int, ll>> s;
+
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	int N;
-	cin >> N;
-	vector<int>v(N);
+    int N;
+    cin >> N;
 
-	for (int i = 0; i < N; i++) {
-		cin >> v[i];
-	}
+    v.resize(N);
 
-	stack < tuple<int, ll, ll, ll >> s;
-	ll sum = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
 
-	for (int i = N - 1; i >= 0; i--) {
-		if (s.empty()) {
-			s.push({ v[i], 0, 0 ,0 });
-			continue;
-		}
+    s.emplace(v[0], 1);
+    long long sol = 0;
+    for (int i = 1; i < N; i++) {
+        ll cnt = 0;
+        while (1) {
+            if (s.empty()) {
+                sol += cnt;
+                s.emplace(v[i], cnt + 1);
+                break;
+            }
 
-		auto [t, big, same, small] = s.top();
-		
-		if (t < v[i]) {
-			ll cnt = 0;
+            auto [t, num] = s.top();
 
-			int x1 = 0, x2 = 0, x3 = 0;
-			while (!s.empty()) {
-				auto [tt, x, y, z] = s.top();
+            if (t > v[i]) {
+                sol += cnt + 1;
+                s.emplace(v[i], cnt + 1);
+                break;
+            } else if (t < v[i]) {
+                sol += num;
+                s.pop();
+            } else {
+                cnt += num;
+                s.pop();
+            }
+        }
+    }
 
-				if (tt > v[i]) {
-					x1 = 1;
-					cnt++;
-					break;
-				}
-
-				if (tt == v[i]) {
-					x2 += (y + 1);
-				}
-				else {
-					x3 += (y + 1);
-				}
-				s.pop();
-			}
-			sum += x1;
-			sum += x2;
-			sum += x3;
-			s.push({ v[i], x1, x2, x3 });
-		}
-		else if (t == v[i]) {
-			ll tmp = big + same + 1;
-			sum += tmp;
-			s.pop();
-			s.push({ v[i], big, same + 1, 0 });
-		}
-		else {
-			sum++;
-			s.push({ v[i], 1, 0 , 0 });
-		}
-	}
-	cout << sum << '\n';
-	return 0;
+    cout << sol << '\n';
+    return 0;
 }
