425bf86ddbbb4b61b559b5149e8eaf5b105545a3
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-03-24 01:50:48 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,58 +1,40 @@
-#include<iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int arr[10001];
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	int front = 0;
-	int rear = 0;
+    queue<int> q;
 
+    int N;
+    cin >> N;
+    while (N--) {
+        string cmd;
+        int x;
+        cin >> cmd;
 
-	int N;
-	cin >> N;
-	while (N--) {
-		string cmd;
-		cin >> cmd;
-
-		if (cmd == "push") {
-			int n;
-			cin >> n;
-			arr[rear++] = n;
-		}
-		else if (cmd == "front") {
-			if (front == rear && !arr[front]) {
-				cout << -1 <<'\n';
-			}
-			else {
-				cout << arr[front] <<'\n';
-			}
-		} else if (cmd == "back") {
-			if (front == rear && !arr[front]) {
-				cout << -1 << '\n';
-			}
-			else {
-				cout << arr[rear-1] << '\n';
-			}
-		}
-		else if (cmd == "size") {
-			cout << rear -front <<'\n';
-		}
-		else if (cmd == "empty") {
-			int result = (front == rear&& !arr[front]) ? 1 : 0;
-			cout << result  <<'\n';
-		}
-		else {
-			if (front == rear && !arr[front]) {
-				cout << -1 << '\n';
-			}
-			else {
-				cout << arr[front++] <<'\n';
-			}
-		}
-	}
+        if (cmd == "push") {
+            cin >> x;
+            q.push(x);
+        } else if (cmd == "pop") {
+            if (q.empty()) {
+                cout << -1 << '\n';
+            } else {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        } else if (cmd == "size") {
+            cout << q.size() << '\n';
+        } else if (cmd == "empty") {
+            cout << (q.empty() ? 1 : 0) << '\n';
+        } else if (cmd == "front") {
+            cout << (q.empty() ? -1 : q.front()) << '\n';
+        } else {
+            cout << (q.empty() ? -1 : q.back()) << '\n';
+        }
+    }
+    return 0;
 }
