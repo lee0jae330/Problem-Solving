35cc0fe8456641791f0b9af23f905fd674e9f562
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-03-27 20:16:13 +0900

    [Gold V] Title: 괄호의 값, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.cc
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.cc
@@ -1,74 +1,105 @@
-#include<iostream>
-#include<string>
-#include<stack>
+#include <bits/stdc++.h>
+
+typedef long long ll;
 
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	string str;
-	cin >> str;
-	stack<pair<char, int>>s;
-	for (char c : str) {
-		if (s.empty()) {
-			if (c == '(' || c == '[') {
-				s.push({ c, 0 });
-			}
-			else {
-				cout << 0 << '\n';
-				return 0;
-			}
-		}
-		else {
-			if (c == '(' || c == '[') {
-				s.push({ c, 0 });
-			}
-			else {
-				char start = c == ')' ? '(' : '[';
-				int tmp = c == ')' ? 2 : 3;
-				int num = 0;
-				int flag = 0;
-				while (!s.empty()) {
-					auto [ch, n] = s.top();
-					s.pop();
-					if (ch == ' ') {
-						num += n;
-					}
-					else if (ch == start) {
-						if (!num) {
-							s.push({ ' ', tmp });
-						}
-						else {
-							s.push({ ' ', tmp * num });
-						}
-						flag = 1;
-						break;
-					}
-					else {
-						cout << 0 << '\n';
-						return 0;
-					}
-				}
-				if (!flag) {
-					cout << 0 << '\n';
-					return 0;
-				}
-			}
-		}
-	}
-	long long sol = 0;
-	while (!s.empty()) {
-		auto [ch, n] = s.top();
-		s.pop();
-		if (ch != ' ') {
-			cout << 0 << '\n';
-			return 0;
-		}
-		sol += n;
-	}
-	cout << sol << '\n';
-	return 0;
+    string str;
+    cin >> str;
+
+    stack<string> s;
+
+    ll sol = 0;
+
+    for (char c : str) {
+        string tmp(1, c);
+
+        if (s.empty()) {
+            if (!(tmp == "(" || tmp == "[")) {
+                cout << 0 << '\n';
+                return 0;
+            }
+            s.push(tmp);
+            continue;
+        }
+
+        if (tmp == "(" || tmp == "[") {
+            s.push(tmp);
+            continue;
+        }
+
+        ll sum = 0;
+        if (tmp == ")") {
+            while (1) {
+                if (s.empty()) {
+                    cout << 0 << '\n';
+                    return 0;
+                }
+
+                string t = s.top();
+
+                if (t == "(") {
+                    if (!sum) {
+                        sum += 2;
+                    } else {
+                        sum *= 2;
+                    }
+                    s.pop();
+                    break;
+                } else if (t == "[") {
+                    cout << 0 << '\n';
+                    return 0;
+                }
+                ll num = stoll(t);
+                sum += num;
+                s.pop();
+            }
+            s.push(to_string(sum));
+        } else {
+            while (1) {
+                if (s.empty()) {
+                    cout << 0 << '\n';
+                    return 0;
+                }
+
+                string t = s.top();
+
+                if (t == "[") {
+                    if (!sum) {
+                        sum += 3;
+                    } else {
+                        sum *= 3;
+                    }
+                    s.pop();
+                    break;
+                } else if (t == "(") {
+                    cout << 0 << '\n';
+                    return 0;
+                }
+                ll num = stoll(t);
+                sum += num;
+                s.pop();
+            }
+            s.push(to_string(sum));
+        }
+    }
+
+    while (!s.empty()) {
+        string t = s.top();
+        if (t[0] >= '0' && t[0] <= '9') {
+            sol += stoll(t);
+        } else {
+            cout << 0 << '\n';
+            return 0;
+        }
+        s.pop();
+    }
+
+    cout << sol << '\n';
+    return 0;
 }
