af00ab94ba31b282fe436003069fd70eaf90800e
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-03-27 18:54:42 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2488 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -1,39 +1,46 @@
-#include<iostream>
-#include<string>
-#include<stack>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	int N;
-	cin >> N;
-	int cnt = 0;
-	while (N--) {
-		string str;
-		cin >> str;
-		stack<char>s;
-		int flag = 0;
-		for (char c : str) {
-			if (s.empty()) {
-				s.push(c);
-			}
-			else {
-				if (s.top() == c) {
-					s.pop();
-				}
-				else {
-					s.push(c);
-				}
-			}
-		}
-		if (s.empty()) {
-			cnt++;
-		}
-	}
-	cout << cnt << '\n';
-	return 0;
+    int N;
+    cin >> N;
+
+    int cnt = 0;
+    while (N--) {
+        string str;
+        cin >> str;
+
+        stack<int> s;
+
+        for (char c : str) {
+            if (s.empty()) {
+                s.push(c);
+            } else {
+                if (c == 'A') {
+                    if (s.top() == 'A') {
+                        s.pop();
+                    } else {
+                        s.push(c);
+                    }
+                } else {
+                    if (s.top() == 'B') {
+                        s.pop();
+                    } else {
+                        s.push(c);
+                    }
+                }
+            }
+        }
+
+        if (s.empty()) {
+            cnt++;
+        }
+    }
+    cout << cnt << '\n';
+    return 0;
 }
