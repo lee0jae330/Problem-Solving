3e5e99be6c685b2b2e35554976943a856abdc8c8
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-03-27 20:27:46 +0900

    [Silver I] Title: 그림, Time: 24 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,62 +1,59 @@
-#include<iostream>
-#include<queue>
-
-int N, M;
-int arr[501][501];
-int visited[501][501];
-int xx[4] = { 0,1,0,-1 };
-int yy[4] = { 1,0,-1,0 };
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int bfs(int x, int y) {
-	queue<pair<int, int>>q;
-	q.emplace(x, y);
-	visited[x][y] = 1;
-	int cnt = 0;
-	while (!q.empty()) {
-		auto[x, y] = q.front();
-		q.pop();
-		cnt++;
-		for (int i = 0; i < 4; i++) {
-			int tx = x + xx[i];
-			int ty = y + yy[i];
-			if (tx < 0 || ty < 0 || tx >= N || ty >= M || visited[tx][ty]) {
-				continue;
-			}
+int N, M;
 
-			if (arr[tx][ty]) {
-				q.emplace(tx, ty);
-				visited[tx][ty] = 1;
-			}
-		}
-	}
-	return cnt;
+int arr[501][501];
+int visited[501][501];
+
+int xx[4] = {0, 1, 0, -1};
+int yy[4] = {1, 0, -1, 0};
+
+int bfs(int x, int y) {
+    int cnt = 0;
+    queue<pair<int, int>> q;
+    q.emplace(x, y);
+    visited[x][y] = 1;
+    while (!q.empty()) {
+        tie(x, y) = q.front();
+        q.pop();
+        cnt++;
+
+        for (int i = 0; i < 4; i++) {
+            int tx = x + xx[i], ty = y + yy[i];
+            if (tx < 0 || ty < 0 || tx >= N || ty >= M || visited[tx][ty] || !arr[tx][ty]) {
+                continue;
+            }
+
+            q.emplace(tx, ty);
+            visited[tx][ty] = 1;
+        }
+    }
+    return cnt;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> arr[i][j];
+        }
+    }
 
-	cin >> N >> M;
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> arr[i][j];
-		}
-	}
-	int sol = 0;
-	int cnt = 0;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (arr[i][j] && !visited[i][j]) {
-				sol = max(sol, bfs(i, j));
-				cnt++;
-			}
-		}
-	}
-	cout << cnt << '\n';
-	cout << sol << '\n';
-	return 0;
+    int cnt = 0;
+    int sol = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (arr[i][j] && !visited[i][j]) {
+                cnt++;
+                sol = max(sol, bfs(i, j));
+            }
+        }
+    }
+    cout << cnt << '\n' << sol << '\n';
+    return 0;
 }
