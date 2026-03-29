91da62de62a1513d509727657159fddb5ce22611
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-03-30 03:45:22 +0900

    [Gold III] Title: 텀 프로젝트, Time: 472 ms, Memory: 4632 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -6,48 +6,50 @@ int N;
 
 int arr[100001];
 int visited[100001];
-int visited2[100001];
+int finished[100001];
 
+int sol;
 
+void dfs(int x) {
+    visited[x] = 1;
+    int nxt = arr[x];
 
-void dfs(int cur, int &cnt) {
-	visited[cur] = 1;
-	int nxt = arr[cur];
-	if (!visited[nxt]) {
-		dfs(nxt, cnt);
-	}
-	else if (!visited2[nxt]) {
-		for (int i = nxt; i != cur; i = arr[i]) {
-			cnt++;
-		}
-		cnt++;
-	}
-	visited2[cur]=1;
+    if (!visited[nxt]) {
+        dfs(nxt);
+    } else if (!finished[nxt]) {
+        // 사이클 끝났는지에 대한 여부
+        for (int cur = nxt; cur != x; cur = arr[cur]) {
+            sol++;
+        }
+        sol++;
+    }
+    finished[x] = 1;
 }
 
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int T;
+    cin >> T;
+    while (T--) {
+        cin >> N;
 
-	int T;
-	cin >> T;
-	while (T--) {
-		memset(arr, 0, sizeof(arr));
-		memset(visited, 0, sizeof(visited));
-		memset(visited2, 0, sizeof(visited2));
-		cin >> N;
-		for (int i = 1; i <= N; i++) {
-			cin >> arr[i];
-		}
+        sol = 0;
+        memset(visited, 0, sizeof(visited));
+        memset(finished, 0, sizeof(finished));
 
-		int cnt =0;
-		for (int i = 1; i <= N; i++) {
-			if (!visited[i]) {
-				dfs(i,cnt);
-			}
-		}
-		cout << N - cnt << '\n';
-	}
+        for (int i = 1; i <= N; i++) {
+            cin >> arr[i];
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+
+        cout << N - sol << '\n';
+    }
+    return 0;
 }
