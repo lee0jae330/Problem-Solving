0e392fb1a61f481661befbfb742d2050fd5718ff
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-03-30 01:43:02 +0900

    [Gold IV] Title: 불, Time: 60 ms, Memory: 9088 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.cc
+++ 백준/Gold/5427. 불/불.cc
@@ -1,94 +1,92 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-
-char arr[1001][1001];
-
-int fire[1001][1001];
-int visited[1001][1001];
-
-int xx[4]={0,1,0,-1};
-int yy[4]={1,0,-1,0};
-
 int w, h;
 
-queue<pair<int,int>>q;
+char arr[1001][1001];
+int visited[1001][1001];
 
-void bfs1() {
-	while (!q.empty()) {
-		auto[x,y] = q.front();
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int tx = x+xx[i], ty = y+yy[i];
-			if(tx<0||tx>=h||ty<0||ty>=w)
-				continue;
-			if(fire[tx][ty]!=-1)
-				continue;
-			if(arr[tx][ty]=='#')
-				continue;
+int xx[4] = {0, 1, 0, -1};
+int yy[4] = {1, 0, -1, 0};
 
-			fire[tx][ty] = fire[x][y]+1;
-			q.push({tx,ty});
-		}
-	}
-}
-
-void bfs(int x, int y) {
-	queue<pair<int,int>>q;
-	q.push({x,y});
-	visited[x][y]=0;
-	while (!q.empty()) {
-		tie(x,y) = q.front();
-		if (x == 0 || x == h - 1 || y == 0 || y == w - 1) {
-			cout << visited[x][y]+1 <<'\n';
-			return;
-		}
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int tx = x+xx[i],ty=y+yy[i];
-			if(tx<0||tx>=h||ty<0||ty>=w)
-				continue;
-			if(visited[tx][ty]!=-1)
-				continue;
-			if (arr[tx][ty] == '.') {
-				if (visited[x][y] + 1 < fire[tx][ty]||fire[tx][ty]==-1) {
-					visited[tx][ty] = visited[x][y]+1;
-					q.push({tx,ty});
-				}
-			}
-		}
-	}
-	cout << "IMPOSSIBLE" <<'\n';
-}
+queue<pair<int, int>> q;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	int T;
-	cin >> T;
-	while (T--) {
-		cin >> w >> h;
-		for (int i = 0; i < h; i++) {
-			fill(fire[i],fire[i]+w,-1);
-			fill(visited[i],visited[i]+w,-1);
-		}
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-		int x, y;
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				cin >> arr[i][j];
-				if (arr[i][j] == '*') {
-					q.push({i,j});
-					fire[i][j] = 0;
-				}
-				else if (arr[i][j] == '@') {
-					x = i, y = j;
-				}
-			}
-		}
-		bfs1();
-		bfs(x,y);
-	}
-	return 0;
+    int T;
+    cin >> T;
+
+    while (T--) {
+        for (int i = 0; i < 1001; i++) {
+            fill(visited[i], visited[i] + 1001, -1);
+        }
+        queue<pair<int, int>> tmpQ;
+        q = tmpQ;
+
+        cin >> w >> h;
+        int sx, sy;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                cin >> arr[i][j];
+                if (arr[i][j] == '@') {
+                    sx = i, sy = j;
+                    arr[i][j] = '.';
+                } else if (arr[i][j] == '*') {
+                    q.emplace(i, j);
+                    visited[i][j] = 0;
+                    arr[i][j] = '.';
+                }
+            }
+        }
+
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int tx = x + xx[i], ty = y + yy[i];
+                if (tx < 0 || ty < 0 || tx >= h || ty >= w || visited[tx][ty] != -1 || arr[tx][ty] == '#') {
+                    continue;
+                }
+                q.emplace(tx, ty);
+                visited[tx][ty] = visited[x][y] + 1;
+            }
+        }
+
+        q.emplace(sx, sy);
+        visited[sx][sy] = 0;
+
+        int sol = -1;
+        int flag = 0;
+
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int tx = x + xx[i], ty = y + yy[i];
+                if (tx < 0 || ty < 0 || tx >= h || ty >= w) {
+                    sol = visited[x][y] + 1;
+                    flag = 1;
+                    break;
+                }
+
+                if (arr[tx][ty] == '#') {
+                    continue;
+                }
+
+                if (visited[tx][ty] == -1 || visited[tx][ty] > visited[x][y] + 1) {
+                    q.emplace(tx, ty);
+                    visited[tx][ty] = visited[x][y] + 1;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+        cout << (sol == -1 ? "IMPOSSIBLE" : to_string(sol)) << '\n';
+    }
+    return 0;
 }
