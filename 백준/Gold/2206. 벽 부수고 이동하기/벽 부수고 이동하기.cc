a6af34e37155fc87e755db64a2fca9eff6ddb823
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-03-30 00:59:13 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 72 ms, Memory: 12616 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,61 +1,75 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int N, M;
 char arr[1001][1001];
 int visited[1001][1001][2];
 
-int xx[4] = { 0,1,0,-1 };
-int yy[4] = { 1,0,-1,0 };
+int xx[4] = {0, 1, 0, -1};
+int yy[4] = {1, 0, -1, 0};
+
+int N, M;
 
 void bfs(int x, int y) {
-	queue<tuple<int, int, int>>q;
-	q.emplace(x, y, 0);
-	visited[x][y][0] = 1;
-	while (!q.empty()) {
-		auto [x, y, flag] = q.front();
-		q.pop();
-		if (x == N - 1 && y == M - 1) {
-			cout << visited[x][y][flag] <<'\n';
-			return;
-		}
+    int flag = 0;
+    queue<tuple<int, int, int>> q;
+    q.emplace(x, y, flag);
+    visited[x][y][flag] = 1;
 
-		for (int i = 0; i < 4; i++) {
-			int tx = x + xx[i], ty = y + yy[i];
-			if (tx < 0 || ty < 0 || tx >= N || ty >= M) {
-				continue;
-			}
+    while (!q.empty()) {
+        tie(x, y, flag) = q.front();
+        q.pop();
 
-			if (arr[tx][ty] == '1') {
-				if (!flag && !visited[tx][ty][1]) {
-					visited[tx][ty][1] = visited[x][y][flag] + 1;
-					q.emplace(tx, ty, 1);
-				}
-			}
-			else {
-				if (!visited[tx][ty][flag]) {
-					visited[tx][ty][flag] = visited[x][y][flag]+1;
-					q.emplace(tx, ty, flag);
-				}
-			}
-		}
-	}
-	cout << -1 <<'\n';
+        for (int i = 0; i < 4; i++) {
+            int tx = x + xx[i], ty = y + yy[i];
+            if (tx < 0 || ty < 0 || tx >= N || ty >= M) {
+                continue;
+            }
+
+            if (arr[tx][ty] == '1') {
+                if (!flag && visited[tx][ty][!flag] == -1) {
+                    visited[tx][ty][!flag] = visited[x][y][flag] + 1;
+                    q.emplace(tx, ty, !flag);
+                }
+            } else {
+                if (visited[tx][ty][flag] == -1) {
+                    visited[tx][ty][flag] = visited[x][y][flag] + 1;
+                    q.emplace(tx, ty, flag);
+                }
+            }
+        }
+    }
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> arr[i][j];
-		}
-	}
+    for (int i = 0; i < 1001; i++) {
+        for (int j = 0; j < 1001; j++) {
+            fill(visited[i][j], visited[i][j] + 2, -1);
+        }
+    }
 
-	bfs(0, 0);
-	return 0;
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    bfs(0, 0);
+    int sol1 = visited[N - 1][M - 1][0], sol2 = visited[N - 1][M - 1][1];
+    if (sol1 != -1 && sol2 != -1) {
+        cout << min(sol1, sol2) << '\n';
+    } else if (sol1 != -1) {
+        cout << sol1 << '\n';
+    } else if (sol2 != -1) {
+        cout << sol2 << '\n';
+    } else {
+        cout << sol1 << '\n';
+    }
+
+    return 0;
 }
