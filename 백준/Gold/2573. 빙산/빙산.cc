28fd0595943dd18b76e04c053fe7ca6d0d97fa40
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-03-30 02:42:43 +0900

    [Gold IV] Title: 빙산, Time: 72 ms, Memory: 2964 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.cc
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -1,122 +1,99 @@
-#include<iostream>
-#include<queue>
-#include<tuple>
-#include<cstring>
-#include<memory.h>
-
+#include <bits/stdc++.h>
 
 using namespace std;
-
 
 int N, M;
 
 int arr[301][301];
 int visited[301][301];
-int cp[301][301];
 
-int xx[4] = { 0,1,0,-1 };
-int yy[4] = { 1,0,-1,0 };
-
-bool checkMelt() {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (arr[i][j]) {
-				return false;
-			}
-		}
-	}
-	return true;
-}
+int xx[4] = {0, 1, 0, -1};
+int yy[4] = {1, 0, -1, 0};
 
 void bfs(int x, int y) {
-	queue<pair<int, int>>q;
-	q.emplace(x, y);
-	visited[x][y] = 1;
-	while (!q.empty()) {
-		tie(x, y) = q.front();
-		q.pop();
+    queue<pair<int, int>> q;
+    q.emplace(x, y);
+    visited[x][y] = 1;
+    while (!q.empty()) {
+        tie(x, y) = q.front();
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int tx = x + xx[i], ty = y + yy[i];
+            if (tx < 0 || ty < 0 || tx >= N || ty >= M || visited[tx][ty] || !arr[tx][ty]) {
+                continue;
+            }
+            q.emplace(tx, ty);
+            visited[tx][ty] = 1;
+        }
+    }
+}
 
-		for (int i = 0; i < 4; i++) {
-			int tx = x + xx[i];
-			int ty = y + yy[i];
+void melt() {
+    int cp[301][301] = {
+        0,
+    };
 
-			if (tx < 0 || ty < 0 || tx >= N || ty >= M || visited[tx][ty]) {
-				continue;
-			}
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            int cnt = 0;
+            if (arr[i][j]) {
+                for (int k = 0; k < 4; k++) {
+                    int tx = i + xx[k], ty = j + yy[k];
+                    if (!arr[tx][ty]) {
+                        cnt++;
+                    }
+                }
+            }
+            cp[i][j] = max(arr[i][j] - cnt, 0);
+        }
+    }
 
-			if (arr[tx][ty]) {
-				visited[tx][ty] = 1;
-				q.emplace(tx, ty);
-			}
-		}
-	}
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            arr[i][j] = cp[i][j];
+        }
+    }
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	cin >> N >> M;
+    cin >> N >> M;
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> arr[i][j];
-		}
-	}
-	int year = 0;
-	while (!checkMelt()) {
-		memset(visited, 0, sizeof(visited));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> arr[i][j];
+        }
+    }
 
-		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (arr[i][j] && !visited[i][j]) {
-					bfs(i, j);
-					cnt++;
-				}
-			}
-		}
+    int sol = 0;
+    while (1) {
+        memset(visited, 0, sizeof(visited));
 
-		if (cnt > 1) {
-			cout << year << '\n';
-			return 0;
-		}
+        int cnt = 0;
 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (arr[i][j] && !visited[i][j]) {
+                    bfs(i, j);
+                    cnt++;
+                }
+            }
+        }
 
+        if (!cnt) {
+            cout << 0 << '\n';
+            return 0;
+        } else if (cnt >= 2) {
+            cout << sol << '\n';
+            return 0;
+        }
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				cp[i][j] = arr[i][j];
-			}
-		}
+        melt();
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (arr[i][j]) {
-					for (int k = 0; k < 4; k++) {
-						int tx = i + xx[k];
-						int ty = j + yy[k];
-						if (tx < 0 || ty < 0 || tx >= N || ty >= M) {
-							continue;
-						}
-
-						if (!arr[tx][ty]) {
-							cp[i][j]--;
-						}
-					}
-					cp[i][j] = max(cp[i][j], 0);
-				}
-			}
-		}
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				arr[i][j] = cp[i][j];
-			}
-		}
-		year++;
-	}
-	cout << 0 << '\n';
-	return 0;
+        sol++;
+    }
+    return 0;
 }
