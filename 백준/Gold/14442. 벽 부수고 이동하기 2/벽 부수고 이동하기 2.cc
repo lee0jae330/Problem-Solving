774140d1c66d5e30b5303fe8ff7052834c7859ab
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-03-30 17:34:20 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 504 ms, Memory: 47840 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M, K;
+char arr[1001][1001];
+int visited[1001][1001][11];
+
+int xx[4] = {0, 1, 0, -1};
+int yy[4] = {1, 0, -1, 0};
+
+void bfs(int x, int y) {
+    int cnt = 0;
+    queue<tuple<int, int, int>> q;
+    q.emplace(x, y, cnt);
+    visited[x][y][cnt] = 1;
+
+    while (!q.empty()) {
+        tie(x, y, cnt) = q.front();
+        if (x == N - 1 && y == M - 1) {
+            cout << visited[x][y][cnt] << '\n';
+            return;
+        }
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int tx = x + xx[i], ty = y + yy[i];
+            if (tx < 0 || ty < 0 || tx >= N || ty >= M) {
+                continue;
+            }
+
+            if (arr[tx][ty] == '0' && !visited[tx][ty][cnt]) {
+                visited[tx][ty][cnt] = visited[x][y][cnt] + 1;
+                q.emplace(tx, ty, cnt);
+            } else if (cnt + 1 <= K && (!visited[tx][ty][cnt + 1] || visited[tx][ty][cnt + 1] > visited[x][y][cnt] + 1)) {
+                visited[tx][ty][cnt + 1] = visited[x][y][cnt] + 1;
+                q.emplace(tx, ty, cnt + 1);
+            }
+        }
+    }
+    cout << -1 << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N >> M >> K;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    bfs(0, 0);
+    return 0;
+}
