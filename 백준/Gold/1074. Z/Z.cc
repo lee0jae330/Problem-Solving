2cdeaabfc75a50476da1beffb597f5e46cefd02b
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-04-06 01:49:53 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -1,26 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int rec(int n, int r, int c){
-    if(n==0) //base condition
-        return 0;
-    int half = 1<<n-1;
-    if(r<half&&c<half) //1사분면
-        return rec(n-1,r,c);
-    else if(r<half&&c>=half)
-        return half*half+rec(n-1,r,c-half);
-    else if(r>=half&&c<half)
-        return 2*half*half+rec(n-1,r-half,c);
-    else
-        return 3*half*half+rec(n-1,r-half,c-half);
+int N, r, c;
+
+int rec(int n, int x, int y) {
+    if (n == 1) {
+        return 2 * x + y;
+    }
+
+    int tmp = (1 << (n - 1));
+    return (2 * (x / tmp) + y / tmp) * (1 << (2 * n - 2)) + rec(n - 1, x % tmp, y % tmp);
 }
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n , r, c;
-    cin >> n >> r >> c;
-    cout << rec(n,r,c);
+    cout.tie(nullptr);
+
+    cin >> N >> r >> c;
+
+    cout << rec(N, r, c) << '\n';
+
     return 0;
 }
