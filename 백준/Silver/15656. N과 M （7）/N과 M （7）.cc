db3e6e372fb91037ece57f935f4d2ceee93c42f3
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-04-08 02:09:46 +0900

    [Silver III] Title: N과 M (7), Time: 392 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -1,43 +1,43 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int N, M;
 
-vector<int>v;
-vector<int>arr;
+vector<int> arr, v;
 
-void backtrack() {
-	if (arr.size() == M) {
-		for (int num : arr) {
-			cout << num << ' ';
-		}
-		cout <<'\n';
-		return;
-	}
+void rec(int cnt) {
+    if (cnt == M) {
+        for (int num : v) {
+            cout << num << ' ';
+        }
+        cout << '\n';
+        return;
+    }
 
-	for (int num : v) {
-		arr.push_back(num);
-		backtrack();
-		arr.pop_back();
-	}
+    for (int i = 0; i < N; i++) {
+        v.push_back(arr[i]);
+        rec(cnt + 1);
+        v.pop_back();
+    }
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-	cin >> N >> M;
-	
-	v.resize(N);
-	
-	for (int i = 0; i < N; i++) {
-		cin >> v[i];
-	}
-	sort(v.begin(), v.end());
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	backtrack();
-	return 0;
+    cin >> N >> M;
+
+    arr.resize(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+
+    rec(0);
+
+    return 0;
 }
