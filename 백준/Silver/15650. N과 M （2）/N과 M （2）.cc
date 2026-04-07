e3f16c89ceb28ab54d29f9b5ab91224f55ad7d33
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-04-08 01:48:13 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,35 +1,40 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include <bits/stdc++.h>
 
-int arr[9];
-int visited[9] = { 0, };
+using namespace std;
+
 int N, M;
 
-void dfs(int num, int count)
-{
-	if (count == M)
-	{
-		for (int i = 0; i < M; i++)
-		{
-				printf("%d ", arr[i]);
-		}
-		printf("\n");
-	}
-	for (int i = num; i <= N; i++)
-	{
-		if (!visited[i])
-		{
-			visited[i] = 1;
-			arr[count] = i;
-			dfs(i+1, count + 1);
-			visited[i] = 0;
-		}
-	}
+int visited[9];
+
+vector<int> v;
+
+void rec(int x, int cnt) {
+    if (cnt == M) {
+        for (int num : v) {
+            cout << num << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = x; i <= N; i++) {
+        if (!visited[i]) {
+            visited[i] = 1;
+            v.push_back(i);
+            rec(i + 1, cnt + 1);
+            v.pop_back();
+            visited[i] = 0;
+        }
+    }
 }
 
-int main(void)
-{
-	scanf("%d %d", &N, &M);
-	dfs(1, 0);
-	return 0;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N >> M;
+
+    rec(1, 0);
+    return 0;
 }
