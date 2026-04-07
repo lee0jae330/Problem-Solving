2e3a1f695325f9dedcabdd15b4f4c660a600c6a2
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-04-08 01:53:33 +0900

    [Silver III] Title: N과 M (3), Time: 340 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -1,34 +1,33 @@
-#include<iostream>
-#include<vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int N,M;
+int N, M;
 
-vector<int>v;
+vector<int> v;
 
-void backtrack(int depth) {
-	if (depth == M) {
-		for (int num : v) {
-			cout << num <<' ';
-		}
-		cout <<'\n';
-		return;
-	}
+void rec(int cnt) {
+    if (cnt == M) {
+        for (int num : v) {
+            cout << num << ' ';
+        }
+        cout << '\n';
+        return;
+    }
 
-	for (int i = 1; i <= N; i++) {
-		v.push_back(i);
-		backtrack(depth+1);
-		v.pop_back();
-	}
+    for (int i = 1; i <= N; i++) {
+        v.push_back(i);
+        rec(cnt + 1);
+        v.pop_back();
+    }
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	cin >> N >> M;
-	backtrack(0);
-	return 0;
+    cin >> N >> M;
+    rec(0);
+    return 0;
 }
