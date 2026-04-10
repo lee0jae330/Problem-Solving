033eba00ee2a2301eb12db7050ed7375ad80fc88
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-04-11 01:30:11 +0900

    [Gold IV] Title: N-Queen, Time: 5472 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,10 +1,48 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
 
-int arr[15]={1,0,0,2,10,4,40,92,352,724,2680,14200,73712,365596,2279184};
+using namespace std;
+
+int N;
+
+int col[15];
+int sol = 0;
+
+bool isValid(int row) {
+    for (int i = 0; i < row; i++) {
+        if (col[row] == col[i]) {
+            return false;
+        }
+
+        if (abs(row - i) == abs(col[row] - col[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void backtrack(int row) {
+    if (row == N) {
+        sol++;
+        return;
+    }
+
+    for (int i = 0; i < N; i++) {
+        col[row] = i;
+        if (isValid(row)) {
+            backtrack(row + 1);
+        }
+    }
+}
 
 int main() {
-	int N;
-	scanf("%d",&N);
-	printf("%d\n",arr[N-1]);
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N;
+
+    backtrack(0);
+
+    cout << sol << '\n';
+    return 0;
 }
